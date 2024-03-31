d011be37c488516ec9e3242b5614db09e42b2245
gamramohamed <162313364+gamramohamed@users.noreply.github.com>
2024-03-31 18:02:32 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+let inp1=document.getElementById('inp1')
+let inp2=document.getElementById('inp2')
+inp1.onkeyup=()=>{
+  localStorage.user=inp1.value
+}
+if (localStorage.length>0) {
+  inp1.value=localStorage.user
+}
